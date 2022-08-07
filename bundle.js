7ac3b0ab54f24f19ae500fca1bfcd0c3f83c0e49
Nina Breznik <ninabreznik@gmail.com>
2022-08-07 19:22:21 +0100

    update font and favicon

--- bundle.js
+++ bundle.js
@@ -2,11 +2,22 @@
 const page = require('..')
 
 function demo () {
+  const font = document.createElement('link')
+  font.setAttribute('href', 'https://fonts.cdnfonts.com/css/magic-school-one-2')
+  font.setAttribute('rel', 'stylesheet')
+
+  const favicon = document.createElement('link')
+  favicon.setAttribute('rel', 'icon')
+  favicon.setAttribute('type', 'imagine/png')
+  favicon.setAttribute('href', './assets/Wizard-Amigos---Stickers---WIZARD-BALL---TRANSPARENT---2022.png')
+  
+  document.head.append(font, favicon)
   const codecamp = page()
   return codecamp
 }
 
 document.body.append(demo())
+
 
 const style = document.createElement('style')
 style.textContent = `
